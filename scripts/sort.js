84c7f6229d3c82d363e411d089c692e495834548
mkadin <mkadin@harleyquinn.edc.org>
2012-11-08 12:45:45 -0500

    ie8 magnifying glass fix

--- scripts/sort.js
+++ scripts/sort.js
@@ -144,7 +144,7 @@ $(function() {
             $modal.dialog("option", "position", "top");
         } else {
             var img = $( "<img class='sort-modal-image' alt='" + title + "' style='display: none; padding: 8px;' />" )
-            .attr( "src", src ).appendTo( "body" );
+            .appendTo( "body" );
             img.load( function () {
                 $(this).dialog({
                     title: $(this).attr('alt'),
@@ -152,7 +152,7 @@ $(function() {
                     modal: true
                 });
                 $(this).dialog("option", "position", "top");
-            });
+            }).attr('src',src);
 
 
         }
