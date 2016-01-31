96aa92eedd1b99dd5185fe984b6f38ce943ea870
Ronchinsky <cronchinsky@vCWeb3.ad.edc.org>
2016-01-31 16:03:34 -0500

    updating so that the popup work correctly

--- lib.php
+++ lib.php
@@ -458,7 +458,8 @@ function sort_get_coursemodule_info($coursemodule) {
     return NULL;
  }
     
-    $info = new stdClass();
+    //$info = new stdClass();
+    $info = new cached_cm_info();
     $info->name = $sort->name;
 	
     if ($sort->display != RESOURCELIB_DISPLAY_POPUP) {
@@ -468,7 +469,8 @@ function sort_get_coursemodule_info($coursemodule) {
     $width  = empty($sort->popupwidth)  ? 620 : $sort->popupwidth;
     $height = empty($sort->popupheight) ? 450 : $sort->popupheight;
     $wh = "width=$width,height=$height,toolbar=no,location=no,menubar=no,copyhistory=no,status=no,directories=no,scrollbars=yes,resizable=yes";
-    $info->extra = "onclick=\"window.open('$fullurl', '', '$wh'); return false;\"";
+    //$info->extra = "onclick=\"window.open('$fullurl', '', '$wh'); return false;\"";
+    $info->onclick = "window.open('$fullurl', '', '$wh'); return false;";
     return $info;
 }
 
